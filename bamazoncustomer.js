9b7c09b9297b696d15f40e881da0e7770b8c02c7
Ekscursionist <larakek@gmail.com>
2018-07-31 10:34:37 -0400

    beginning to build bamazoncustomer (even though it should be shamazoncustomer)

---
+++ bamazoncustomer.js
@@ -0,0 +1,54 @@
+var mysql = require("mysql");
+var inquirer= require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "topsongs_DB"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+    shamazon();  
+    displaystore();
+});
+
+function shamazon() {
+    console.log("it's working!");
+}
+
+function displaystore() {
+    console.log("this function displays the store");
+// 5. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+    whatwouldyouliketobuy();
+}
+
+function whatwouldyouliketobuy() {
+    console.log("A CUSTOMER!!! Wait! What's your rush, what's your hurry? You gave me such I fright! I thought you was a ghost!");
+// 6. The app should then prompt users with two messages.
+//    * The first should ask them the ID of the product they would like to buy.
+//    * The second message should ask how many units of the product they would like to buy.
+// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+//    * This means updating the SQL database to reflect the remaining quantity.
+//    * Once the update goes through, show the customer the total cost of their purchase.
+
+}
+
+
+
+
+
+
+
