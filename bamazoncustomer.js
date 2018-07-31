85eb00e17da0297c0bc87da14c298a17a8c1307f
Ekscursionist <larakek@gmail.com>
2018-07-31 17:00:35 -0400

    inquirer inquires one question.  how to continue questions...

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -49,20 +49,32 @@ function whatwouldyouliketobuy() {
 //    console.log("A CUSTOMER!!! Wait! What's your rush, what's your hurry? You gave me such I fright! I thought you was a ghost!");
 //    console.log("Now!  What would you like to buy?");
     inquirer.prompt ({
-        name: "userbuy",
         type: "input",
-        message: "Now!  What would you like to buy?"
-    })
-// 6. The app should then prompt users with two messages.
-//    * The first should ask them the ID of the product they would like to buy.
-//    * The second message should ask how many units of the product they would like to buy.
-// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+        name: "userbuy",
+        message: "Now, please type the ID number of the object you would like to buy!"
+    },
+    {
+        type: "input",
+        name: "howmany",
+        message: "How many would you like to buy?"
+    }).then(function(inquireresponse){
+        if (inquireresponse.confirm) {
+            console.log("you want " + inquireresponse.howmany + " of these " + inquireresponse.userbuy + "s!")
+        }
+
+    });
+//-- 6. The app should then prompt users with two messages.
+//--    * The first should ask them the ID of the product they would like to buy.
+//--    * The second message should ask how many units of the product they would like to buy.
+// 7. Once the customer has placed the order, your application should check 
+//      if your store has enough of the product to meet the customer's request.
 //    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
 // 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
 //    * This means updating the SQL database to reflect the remaining quantity.
 //    * Once the update goes through, show the customer the total cost of their purchase.
 
 }
+
 
 
 
